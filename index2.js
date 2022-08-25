eeecda3e6adc32136e6a1efd1f90cb7c578ef76b
Juhwa Hwang <zerothhan@gmail.com>
2022-08-25 15:38:36 +0900

    2-3. redux middleware

--- index2.js
+++ index2.js
@@ -1,4 +1,4 @@
-const { createStore } = require("redux");
+const { createStore, compose, applyMiddleware } = require("redux");
 const reducer = require("./reducers");
 const { addPost } = require("./actions/post");
 const { logIn, logOut } = require("./actions/user");
@@ -9,14 +9,22 @@ const initialState = {
     data: null,
   },
   posts: [],
-  comments: [],
-  favorites: [],
-  history: [],
-  likes: [],
-  followers: [],
 };
 
-const store = createStore(reducer, initialState);
+// compose : 합성하는 함수
+// compose 없이도 동작함
+// compose를 하는 이유 : applyMiddleware 말고 redux devtool 같은 것들도 추가적으로 붙일 때 compose로 함수를 합성한다.
+const firstMiddleware = (store) => (dispatch) => (action) => {
+  console.log("action log", action);
+  // dispatch 전 기능 추가
+  dispatch(action);
+  // dispatch 후 기능 추가
+  console.log("action end");
+};
+
+const enhancer = applyMiddleware(firstMiddleware);
+
+const store = createStore(reducer, initialState, enhancer);
 
 // react-redux 안에 들어있음. 실제 react-redux 쓸 일은 거의 없음. 에러 디버깅할 때만 씀
 store.subscribe(() => {
